64e9c739037966f80278a613be2b9ce6855f5f8e
Hassan Khan <hassan-k1@hotmail.com>
2019-02-02 13:03:05 -0500

    Greet Script

--- greet.sh
+++ greet.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-#Bash Script that greets with appropriate message based on current time
+ #Bash Script that greets with appropriate message based on current time
 
  typeset -i clock=0
  user=`whoami`
  val=`date | cut -c 12-19`
- clock=`echo $val | cut -d":" -f1`
+ hour=`echo $val | cut -c 1-2`
 
  #echo $val
- #echo $clock
+ #echo $hour
 
- if [[ $clock -gt 20  || $clock -le 02 ]]
+ if [[ $hour -ge 20  &&  $hour -le 24  ]]
    then
         echo "Good Night $user"
 
-   elif [[ $clock -gt 12 && $clock -le 17  ]]
+   elif [[ $hour -ge 12 && $hour -le 17  ]]
    then
         echo " Good Afternoon $user"
 
-   elif [[      $clock -ge 17 && $clock -le 20  ]]
+   elif [[  $hour -ge 17 && $hour -le 20  ]]
    then
         echo "Good Evening $user"
 
-   elif [[ $clock -gt 02  && $clock -le 12  ]]
+   elif [[ $hour -gt 0  && $clock -le 12  ]]
    then
         echo "Good Morning $user "
  fi
