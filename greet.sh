e2c8b840d8a8dfb2d06925fb139626d66b94a21d
Hassan Khan <44042104+Ha55an-k1@users.noreply.github.com>
2019-03-09 16:59:08 -0500

    Update greet.sh

--- greet.sh
+++ greet.sh
@@ -6,9 +6,18 @@
  user=`whoami`
  val=`date | cut -c 12-19`
  hour=`echo $val | cut -c 1-2`
-
+ dig1="${hour:0:1}"
+ dig2="${hour:1:2}"
+ #echo $dig1
+ #echo $dig2
  #echo $val
  #echo $hour
+
+ #digit check for morning
+ if [ $dig1 -eq "0" ]
+ then
+ hour=$dig2
+ fi
 
  if [[ $hour -ge 20  &&  $hour -le 24  ]]
    then
@@ -22,7 +31,7 @@
    then
         echo "Good Evening $user"
 
-   elif [[ $hour -gt 0  && $clock -le 12  ]]
+   elif [[ $hour -ge 0  && $clock -le 12  ]]
    then
         echo "Good Morning $user "
  fi
